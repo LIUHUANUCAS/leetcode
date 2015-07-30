c05b0cc85c4fa99813878e5c10f15389c3abcfc2
LIUHUAN <liu2012huan@126.com>
2015-07-30 17:23:18 +0800

    add stack solution

---
+++ Solutions/Stack/000stack.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include <cstdlib>
+#include<iterator>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+
+
+/**
+    doc
+
+
+
+    name.cpp
+*/
+
+class Solution {
+
+       public:
+
+};
+
+int main(){
+        Solution s;
+
+}
