85fd38e022669d8cda5fc50bd0a3f96d72cd09bc
liuhuan <liuhuan@ict.ac.cn>
2015-10-02 20:13:49 +0800

    update 219

--- Solutions/Hashtable/219ContainsDuplicateII.cpp
+++ Solutions/Hashtable/219ContainsDuplicateII.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 #include <cstdlib>
 #include<iterator>
+#include<map>
+#include<unordered_map>
 using namespace std;
 
 
@@ -19,7 +21,43 @@ class Solution {
        public:
        
         bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        
+            
+            unordered_map<int,int> hmap;
+            size_t n = nums.size();
+            if(n <= 1)
+                return 0;
+            if(k <=0)
+                return 0;
+            hmap[nums[0]] = 0;
+            for(size_t i =1;i< n ; ++i){
+               int a = nums[i];
+               
+               if(hmap[a] == 0){//
+                   if(a == nums[0]){
+                       if(i-0 <= k ){
+                           
+                       // cout<<i<<"->"<<hmap[a]<<endl;
+                           
+                       return 1;
+                       }
+                   } 
+                   hmap[a] = i;
+                   
+               }else{
+                   if(i - hmap[a] <= k){
+                       
+                       // cout<<i<<"->"<<hmap[a]<<endl;
+                        return 1;
+                   }
+                   else
+                        hmap[a] = i;
+                   
+               }
+               
+               
+            }//for 
+            return 0;
+      
         }
 
     
@@ -31,7 +69,22 @@ class Solution {
 
 int main(){
         Solution s;
-
+        
+        vector<int> nums = {
+            // 1,2,3,-1,5,4,3,-1,6,3
+            -1,-1
+            };
+        int k=4;
+        unordered_map<int,int> map1;
+        map<int,int> mp;
+        cout<<mp[0]<<endl;
+        int a = map1[1];
+        // cout<<a<<endl;
+        // return 0;
+        cin >> k;
+        bool res = s.containsNearbyDuplicate(nums,k);
+        cout<<res<<endl;
+        return 0;
       
 
 }
